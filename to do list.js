ec051922729cd8aa8f3726ebf4286681efae5c14
Your Name <you@example.com>
2018-12-21 11:27:11 +0100

    changed displayTodos function

--- to do list.js
+++ to do list.js
@@ -1,9 +1,13 @@
 var todoList = {
   todos: [],
   displayTodos: function(){
+    if (this.todos.length === 0){
+      console.log("Your todo list is empty!");
+    }else {
     console.log("My Todos:");
-    for (var = 0; i < this.todos.length; i++){
+    for (var i = 0; i < this.todos.length; i++){
       console.log(this.todos[i].todoText);
+    }  
     }
   },
   addTodo: function(todoText){
